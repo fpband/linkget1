80c429436a34317ccd2742bc4f002954042f6654
fpband <82159763+fpband@users.noreply.github.com>
2022-11-14 17:40:17 +0330

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    API_ID = int( os.getenv("api_id","1234") )
-    API_HASH = os.getenv("api_hash","21ab7cb0a453b5e60016dc7bbeb71cb")
-    CHANNEL = int( os.getenv("channel_files_chat_id","-1001601419165") )
-    CHANNEL_USERNAME = os.getenv("channel_username","UserLandapp")
-    TOKEN = os.getenv("token","1408542385:AAEatXPbSubsYmlW8kPTUAr")
-    DOMAIN  = os.getenv("domain","http://localhost")
+    API_ID = int( os.getenv("api_id","3335796") )
+    API_HASH = os.getenv("api_hash","138b992a0e672e8346d8439c3f42ea78")
+    CHANNEL = int( os.getenv("channel_files_chat_id","-1001645543494") )
+    CHANNEL_USERNAME = os.getenv("channel_username","dlchin")
+    TOKEN = os.getenv("token","1996499923:AAGVCjT4msY1AOHb-qtalwMsrwu0dD65k7g")
+    DOMAIN  = os.getenv("domain","https://newfastlink7.osc-fr1.scalingo.io/")
